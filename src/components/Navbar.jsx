38d4d756582158d330d18b4b85b9465dfb0eb4c1
Zachary Soliman <zachsoliman1@gmail.com>
2023-12-10 11:13:03 -0500

    navbar setup done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+// import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+    return (
+        <div className='Navbar'>
+
+            {/* <Link
+                className='Link' to='/about'>
+                <a>About</a>
+            </Link>
+
+            <Link
+                className='Link' to='/still-gallery'>
+                <a>Photos</a>
+            </Link>
+
+            <Link
+                className='Link' to='/moving-gallery'>
+                <a>Hours</a>
+            </Link>
+
+            <Link
+                className='Link' to='/contact'>
+                <a>Contact</a>
+            </Link> */}
+
+            <a>about</a>
+            <a>photos</a>
+            <a>hours</a>
+            <a>contact</a>
+
+
+        </div>
+    )
+}
+
+export default Navbar;
