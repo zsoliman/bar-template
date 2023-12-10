4eaf1291531aed224a032bc00dd46ec68a622173
Zachary Soliman <zachsoliman1@gmail.com>
2023-12-10 11:47:28 -0500

    header with fonts

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className="Header">
+            <h1>Zach Pours Drinks</h1>
+            <h2>come try one !</h2>
+        </div>
+    )
+}
+
+export default Header;
