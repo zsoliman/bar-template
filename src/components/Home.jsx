991b50a0bc2fd96b3915ab165e657b15bf546e0d
Zachary Soliman <zachsoliman1@gmail.com>
2023-12-10 19:26:55 -0500

    home page basics

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,19 +3,29 @@ import { Link } from 'react-router-dom'
 import Navbar from "./Navbar";
 import Header from "./Header";
 import Footer from './Footer';
+import mojito3 from "./images/mojito3.png";
 
 const Home = () => {
     return (
         <div className="Home">
             <Navbar />
             <Header />
-
             <h2>come try one !</h2>
 
-            <Link
-                className='Link' to='/about'>
-                <button className='EnterButton'>enter &nbsp; ></button>
-            </Link>
+            <div className='HomeBody'>
+
+                <img className="HomeImg" src={mojito3} />
+                <h1>
+                    Brooklyn<br />Bars<br /> Baby
+                </h1>
+
+
+                {/* <Link
+                    className='Link' to='/about'>
+                    <button className='EnterButton'>enter &nbsp; ></button>
+                </Link> */}
+
+            </div>
 
             <Footer />
         </div>
